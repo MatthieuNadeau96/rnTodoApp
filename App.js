5b1e6014268268ad63c786c807534f914c445cdf
Matthieu <matthieunadeau96@gmail.com>
2018-12-30 12:56:19 -0500

    Structuring and added Provider to App.js

--- App.js
+++ App.js
@@ -1,28 +1,17 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 
 import TodoApp from './src/TodoApp';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import store from './src/store/index';
+import { Provider } from 'react-redux';
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <TodoApp/>
+      <Provider store={store}>
+        <TodoApp/>
+      </Provider>
     );
   }
 }
@@ -33,15 +22,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  }
 });
