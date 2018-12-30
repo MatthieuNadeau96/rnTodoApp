098b4b7ddd176c67219de66d3cee6d94ffe8f3f9
Matthieu <matthieunadeau96@gmail.com>
2018-12-30 15:34:52 -0500

    Created reducers and Combined them

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -4,12 +4,18 @@ import { View, Text, StyleSheet } from 'react-native';
 import AddTodo from './containers/AddTodo';
 
 class TodoApp extends Component {
+
+  state = {
+    todos: [],
+    visibilityFilter: 'SHOW_ALL'
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <AddTodo/>
         <View>
-          
+
         </View>
       </View>
     )
